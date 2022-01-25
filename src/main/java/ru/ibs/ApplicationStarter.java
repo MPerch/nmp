0e12ffc5cb0e782fbf8dbb13bf5636c53060ffdd
MPerch <MPerchikov@IBS.ru>
2022-01-25 17:55:50 +0300

    без контроллера

--- src/main/java/ru/ibs/ApplicationStarter.java
+++ src/main/java/ru/ibs/ApplicationStarter.java
@@ -1,7 +1,11 @@
 package ru.ibs;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApplicationStarter {
     public static void main(String[] args) {
-        
+        SpringApplication.run(ApplicationStarter.class);
     }
 }
--- src/main/java/ru/ibs/controller/MainRestController.java
+++ src/main/java/ru/ibs/controller/MainRestController.java
@@ -1,2 +1,19 @@
-package ru.ibs.controller;public class MainRestController {
+package ru.ibs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/api")
+public class MainRestController {
+
+    @GetMapping("/line")
+    public String showMeSomeLine(){
+
+        String str = "Some word!";
+        return str;
+    }
 }
